cabd7311a97e83135a6dec55d84a61751ab79a69
prolife256 <41485264+prolife256@users.noreply.github.com>
2019-05-17 02:28:12 +0300

    Add files via upload
    bash calıstırın.sh
    
    yazarak otomati kurulumu gerçekleştirebilirsiniz

---
+++ calıstırın.sh
@@ -0,0 +1,21 @@
+
+ 
+apt update && apt upgrade -y
+
+pkg install git -y
+
+pkg install php -y
+
+pkg install curl -y
+
+pkg install python2 -y
+
+
+
+git clone https://github.com/4L13199/LITESCRIPT
+
+
+termux-setup-storage
+
+
+
